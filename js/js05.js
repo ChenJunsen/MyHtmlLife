d2ea28348099bb73be1f5ad663c6e3383dd98841
ChenJunSen <1534717040@qq.com>
2018-04-10 16:50:14 +0800

    Angular作用域测试

---
+++ js/js05.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Alienware on 2018/4/10 0010.
+ */
+app.controller("testCtrl2",function ($scope) {
+    $scope.myStrs=[
+        {x:"GTA",y:"侠盗猎车"},
+        {x:"TorchLight",y:"火炬之光"},
+        {x:"LegendaryOfGlory",y:"王者荣耀"},
+        {x:"Witcher",y:"巫师"}
+    ];
+});
